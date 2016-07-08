078b711735061cbcc62261c9810ef20d3e17e285
Carel Schoombee <carelschoombee@gmail.com>
2016-07-08 13:22:42 +0200

    Commit 2

---
+++ TestGitHub/Class1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        Console.WriteLine();
+
+        try
+        {
+
+        }
+        catch (Exception)
+        {
+
+                throw;
+        }
+
+                try
+                {
+
+                }
+                finally
+                {
+
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+
+        }
+
+        for (int i = length - 1; i >= 0; i--)
+        {
+
+        }
+
+        foreach (var item in collection)
+        {
+
+        }
+
+        var names = new List<string>();
+        foreach (var name in collection)
+        {
+
+        }
+
+        while (true)
+        {
+
+        }
+
+        do
+        {
+
+        } while (true);
+    }
+
+    private int myVar;
+
+    // override object.Equals
+    public override bool Equals(object obj)
+    {
+        //       
+        // See the full list of guidelines at
+        //   http://go.microsoft.com/fwlink/?LinkID=85237  
+        // and also the guidance for operator== at
+        //   http://go.microsoft.com/fwlink/?LinkId=85238
+        //
+
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        // TODO: write your implementation of Equals() here
+        throw new NotImplementedException();
+        return base.Equals(obj);
+    }
+
+    // override object.GetHashCode
+    public override int GetHashCode()
+    {
+        // TODO: write your implementation of GetHashCode() here
+        throw new NotImplementedException();
+        return base.GetHashCode();
+    }
+
+    public int MyProperty
+    {
+        get { return myVar; }
+        set { myVar = value; }
+    }
+
+}
---
+++ TestGitHub/Class2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Class2
+{
+    public Class2()
+    {
+
+    }
+
+    private int myVar;
+
+    public int MyProperty
+    {
+        get
+        {
+
+            Console.WriteLine
+
+                ("Test");
+
+            return myVar;
+        }
+        set { myVar = value; }
+    }
+
+
+
+
+}
